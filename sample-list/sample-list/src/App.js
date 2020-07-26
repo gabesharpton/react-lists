2c8476404dcae88bd46f54382a9c6333b314d6db
gabesharpton <gabriel.sharpton@gmail.com>
2020-07-26 17:17:05 -0500

    added base react app

---
+++ sample-list/sample-list/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello there</h1>
+      <h2>We are going to make some lists.</h2>
+      <h3>Where shall we begin?</h3>
+    </div>
+  );
+}
+
+export default App;
